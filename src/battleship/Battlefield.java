390edc98501393cec7c3e3c80f675bef7862fc4d
rad <rs.wasiak@gmail.com>
2023-04-11 12:45:42 +0200

    Update files

--- src/battleship/Battlefield.java
+++ src/battleship/Battlefield.java
@@ -1,5 +1,7 @@
 package battleship;
 
+
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.Scanner;
 
@@ -17,7 +19,7 @@ public class Battlefield {
 
     public Battlefield(int size) {
         this.gameFieldSize = size;
-        this.gameField = new String[size + 1][size + 1];
+        this.gameField = new String[size + 2][size + 2];
         for (int i = 0; i < size + 1; ++i) {
             for (int j = 0; j < size + 1; ++j) {
                 if (i == 0) {
@@ -28,6 +30,10 @@ public class Battlefield {
                     else this.gameField[i][j] = "~";
                 }
             }
+        }
+        Arrays.fill(this.gameField[size + 1], "=");
+        for (int row = 0; row < size + 2; ++row) {
+            this.gameField[row][size + 1] = "=";
         }
     }
 
@@ -40,15 +46,18 @@ public class Battlefield {
         Scanner scanner = new Scanner(System.in);
         for (String key : ships.keySet()) {
             System.out.println("Enter the coordinates of the " + key + " (" + ships.get(key) + " cells):");
-            while (true) {
-                System.out.print("> ");
-                String answer = scanner.nextLine();
-                if (checkCoordinates(answer)) break;
-            }
+            String coordinates;
+            do {
+                System.out.print("\n> ");
+                coordinates = scanner.nextLine();
+                System.out.println();
+            } while (!checkCoordinates(coordinates, key));
+            drawShip(coordinates);
+            print();
         }
     }
 
-    private boolean checkCoordinates(String coordinates) {
+    private boolean checkCoordinates(String coordinates, String shipName) {
         // Check if coordinates are in correct format
         if (!(coordinates.matches("^[A-Z]([1-9]|1\\d|2[0-6])\\s[A-Z]([1-9]|1\\d|2[0-6])$"))) {
             System.out.println("Error 1! Wrong ship location! Try again:");
@@ -67,21 +76,94 @@ public class Battlefield {
                 return false;
             }
         }
-        // Check if the ship doesn't touch any other ships
-//        scn = new Scanner(coordinates);
-//        while (scn.hasNext()) {
-//            String next = scn.next();
-//        }
-//
+        // Check if ship doesn't touch any other ships
+        scn = new Scanner(coordinates);
+        int row1, row2, column1, column2;
+
+        String next = scn.next();
+        row1 = next.charAt(0) - 'A' + 1;
+        column1 = Integer.parseInt(next.substring(1));
+
+        next = scn.next();
+        row2 = next.charAt(0) - 'A' + 1;
+        column2 = Integer.parseInt(next.substring(1));
+
+        if (row1 == row2) {
+            int min = Math.min(column1, column2);
+            int max = Math.max(column1, column2);
+            if (!(max - min + 1 == ships.get(shipName))) {  // Check if ship takes up correct number of fields
+                System.out.println("Error 5! Coordinates don't match the number of fields for the current ship! Try again:");
+                return false;
+            }
+            for (int column = min; column <= max; ++column) {  // Check if ship doesn't touch any other ships
+                if (this.gameField[row1][column].contains("O") ||
+                        this.gameField[row1 - 1][column].contains("O") ||
+                        this.gameField[row1 + 1][column].contains("O") ||
+                        this.gameField[row1][column - 1].contains("O") ||
+                        this.gameField[row1][column + 1].contains("O")) {
+                    System.out.println("Error 6! Ship cannot touch any other ship! Try again:");
+                    return false;
+                }
+            }
+        } else if (column1 == column2) {
+            int min = Math.min(row1, row2);
+            int max = Math.max(row1, row2);
+            if (!(max - min + 1 == ships.get(shipName))) {  // Check if ship takes up correct number of fields
+                System.out.println("Error 5! Coordinates don't match the number of fields for the current ship! Try again:");
+                return false;
+            }
+            for (int row = min; row <= max; ++row) {  // Check if ship doesn't touch any other ships
+                if (this.gameField[row][column1].contains("O") ||
+                        this.gameField[row - 1][column1].contains("O") ||
+                        this.gameField[row + 1][column1].contains("O") ||
+                        this.gameField[row][column1 - 1].contains("O") ||
+                        this.gameField[row][column1 + 1].contains("O")) {
+                    System.out.println("Error 6! Ship cannot touch any other ship! Try again:");
+                    return false;
+                }
+            }
+        } else {
+            System.out.println("Error 4! Ships can only be placed in a straight line! Try again:");
+            return false;
+        }
+
         return true;
+    }
+
+    private void drawShip(String coordinates) {
+        Scanner scn = new Scanner(coordinates);
+        int row1, row2, column1, column2;
+
+        String next = scn.next();
+        row1 = next.charAt(0) - 'A' + 1;
+        column1 = Integer.parseInt(next.substring(1));
+
+        next = scn.next();
+        row2 = next.charAt(0) - 'A' + 1;
+        column2 = Integer.parseInt(next.substring(1));
+
+        if (row1 == row2) {
+            int min = Math.min(column1, column2);
+            int max = Math.max(column1, column2);
+            for (int column = min; column <= max; ++column) {
+                gameField[row1][column] = "O";
+            }
+        } else if (column1 == column2) {
+            int min = Math.min(row1, row2);
+            int max = Math.max(row1, row2);
+            for (int row = min; row <= max; ++row) {
+                gameField[row][column1] = "O";
+            }
+        }
     }
 
     private void print() {
         for (int i = 0; i < this.gameFieldSize + 1; ++i) {
             for (int j = 0; j < this.gameFieldSize + 1; ++j) {
-                System.out.printf("%s  ", this.gameField[i][j]);
+                System.out.printf("%s ", this.gameField[i][j]);
             }
             System.out.println();
         }
+        System.out.println();
     }
 }
--- src/battleship/Main.java
+++ src/battleship/Main.java
@@ -3,7 +3,6 @@ package battleship;
 
 public class Main {
     public static void main(String[] args) {
-        // Create empty game field
         Battlefield battle = new Battlefield(10);
         battle.startGame();
     }
